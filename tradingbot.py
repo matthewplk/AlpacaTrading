b33c2841c0adb886d8a56b6b36aa75c61b2a1ce7
Matthew Polk <matthewpolkofficial@gmail.com>
2024-05-23 02:15:04 -0500

    Algorithm Trading Bot Intial Python

--- tradingbot.py
+++ tradingbot.py
@@ -5,6 +5,7 @@ from lumibot.traders import Trader
 from datetime import datetime
 from alpaca_trade_api import REST
 from timedelta import Timedelta
+from finbert_utils import estimate_sentiment
 
 API_KEY = "PKM9U1LB8VFOP92UI4WD"
 API_SECRET = "WFVodtpZoNH3BQ5wz3PdhM1nRQEAegnalOEhXA7i"
@@ -36,22 +37,26 @@ class MLTrader(Strategy):
         return today.strftime('%Y-%m-%d'), three_days_prior.strftime('%Y-%m-%d')
 
 
-    def get_news(self):
+    def get_sentiment(self):
         today, three_days_prior = self.get_dates()
         news = self.api.get_news(symbol=self.symbol,
                                 start=three_days_prior, 
                                 end=today)
         news = [ev.__dict__["_raw"]["headline"] for ev in news]
-        return news
+        probability, sentiment = estimate_sentiment(news)
+
+        return probability,sentiment
 
 
     def on_trading_iteration(self):
         cash, last_price, quantity = self.position_sizing()
+        probability, sentiment = self.get_sentiment()
 
         if cash > last_price:
-            if self.last_trade == None:
-                news = self.get_news()
-                print(news)
+            if sentiment =="positive" and probability > .999:
+                if self.last_trade =="sell":
+                    self.sell_all()
+
                 order = self.create_order(
                     self.symbol,
                     quantity,
@@ -63,16 +68,32 @@ class MLTrader(Strategy):
                 self.submit_order(order)
                 self.last_trade = "buy"
 
+            elif sentiment =="negative" and probability > .999:
+                if self.last_trade =="buy":
+                    self.sell_all()
+                    
+                order = self.create_order(
+                    self.symbol,
+                    quantity,
+                    "sell",
+                    type="bracket",
+                    take_profit_price = last_price*.8,
+                    stop_loss_price= last_price*1.05
+                )
+                self.submit_order(order)
+                self.last_trade = "sell"
+
     
 
 #These propagate for the backtesting period of your algorithm.
-start_date = datetime(2023,12,15)
+start_date = datetime(2022,1,1)
 end_date = datetime(2023,12,31)
 
 broker = Alpaca(ALPACA_CREDITS)
 strategy = MLTrader(name ="mlstrat", broker =broker, parameters={"symbol":"SPY",
                                                                  "cash_at_risk":.5})
 
+# Comment out backtest below when going LIVE!
 strategy.backtest(
     YahooDataBacktesting,
     start_date,
@@ -80,3 +101,8 @@ strategy.backtest(
     parameters = {"symbol":"SPY",
                   "cash_at_risk":.5}
 )
+
+# THESE 3 lines allow the strategy to go LIVE!!
+# trader = Trader()
+# trader.add_strategy(strategy)
+# trader.run_all()
